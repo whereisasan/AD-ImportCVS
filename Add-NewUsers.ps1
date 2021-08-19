c554229586e3bb5c2856a37ea8a3d825e37309d4
Asan <asan@where.email>
2021-08-19 14:32:34 +0500

    Create Add-NewUsers.ps1

---
+++ Add-NewUsers.ps1
@@ -0,0 +1,70 @@
+﻿# Import active directory module for running AD cmdlets
+Import-Module ActiveDirectory
+  
+# Store the data from NewUsersFinal.csv in the $ADUsers variable
+$ADUsers = Import-Csv C:\temp\NewUsersFinal.csv -Delimiter ";"
+
+# Define UPN
+$UPN = "whereis.email"
+
+#Выполните цикл по каждой строке, содержащей сведения о пользователе в файле CSV
+foreach ($User in $ADUsers) {
+
+    #Read user data from each field in each row and assign the data to a variable as below
+    $username = $User.username
+    $password = $User.password
+    $firstname = $User.firstname
+    $lastname = $User.lastname
+    $middlename = $User.middlename
+    $Description = $User.Description
+    $OU = $User.ou #This field refers to the OU the user account is to be created in
+    $email = $User.email
+    $streetaddress = $User.streetaddress
+    $city = $User.city
+    $zipcode = $User.zipcode
+    $state = $User.state
+    $telephone = $User.telephone
+    $jobtitle = $User.jobtitle
+    $company = $User.company
+    $department = $User.department
+
+    # Check to see if the user already exists in AD
+    if (Get-ADUser -F { SamAccountName -eq $username }) {
+        
+        # If user does exist, give a warning
+        Write-Warning "A user account with username $username already exists in Active Directory."
+    }
+    else {
+
+        # User does not exist then proceed to create the new user account
+        # Account will be created in the OU provided by the $OU variable read from the CSV file
+        New-ADUser `
+            -SamAccountName $username `
+            -UserPrincipalName "$username@$UPN" `
+            -Name "$firstname $middlename $lastname" `
+            -GivenName $firstname `
+            -Surname $lastname `
+            -Initials $middlename `
+            -Description $Description `
+            -Enabled $True `
+            -DisplayName "$firstname $middlename $lastname" `
+            -Path $OU `
+            -City $city `
+            -PostalCode $zipcode `
+            -Company $company `
+            -State $state `
+            -StreetAddress $streetaddress `
+            -OfficePhone $telephone `
+            -EmailAddress $email `
+            -Title $jobtitle `
+            -Department $department `
+            -AccountPassword (ConvertTo-secureString $password -AsPlainText -Force) -ChangePasswordAtLogon $False -PasswordNeverExpires $True
+
+        # If user is created, show message.
+        Write-Host "The user account $username is created." -ForegroundColor Cyan
+    }
+}
+
+#-CannotChangePassword $True
+
+Read-Host -Prompt "Press Enter to exit"
